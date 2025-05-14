84f032351e69641a4f5999bca1ac9c58fd17032e
Daniel Danis <daniel.gordon.danis@protonmail.com>
2025-05-14 21:47:52 +0200

    Simplify gene.

--- boqa-core/src/main/java/com/github/p2gx/boqa/core/Gene.java
+++ boqa-core/src/main/java/com/github/p2gx/boqa/core/Gene.java
@@ -1,11 +1,16 @@
 package com.github.p2gx.boqa.core;
 
-import org.monarchinitiative.phenol.ontology.data.Identified;
+import org.monarchinitiative.phenol.ontology.data.TermId;
 
 /**
  * The information about a gene that we need in BOQA.
  */
-public interface Gene extends Identified {
+public interface Gene {
+
+    /**
+     * Get gene's identifier (e.g. <code>HGNC:3603</code> for <em>FBN1</em>).
+     */
+    TermId id();
 
     /**
      * Get HGVS symbol of the gene (e.g. <em>FBN1</em>).
