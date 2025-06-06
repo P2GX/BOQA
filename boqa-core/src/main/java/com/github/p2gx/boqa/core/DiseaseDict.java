7992f87f8cd0e9a5c0a12b80ee23556e53baf1af
hansenp <10495485+hansenp@users.noreply.github.com>
2025-06-06 17:02:18 +0200

    Updates

--- boqa-core/src/main/java/com/github/p2gx/boqa/core/DiseaseDict.java
+++ boqa-core/src/main/java/com/github/p2gx/boqa/core/DiseaseDict.java
@@ -13,33 +13,186 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class DiseaseDict {
     private static final Logger LOGGER = LoggerFactory.getLogger(DiseaseDict.class);
     String phenotypeAnnotationFile;
     String ontologyFile;
+    List<String> validDatabaseList;
     HpoDiseases diseases;
-    HashMap<String, List<String>> diseaseDict;
+    HashMap<String, HashMap<String, Set<String>>> diseaseFeaturesDict;
+    List<String> hpoFreqTermList;
+    List<String> excludedHpoFreqTermList;
 
     public DiseaseDict(String phenotypeAnnotationFile, String ontologyFile) throws IOException{
         this.phenotypeAnnotationFile = phenotypeAnnotationFile;
         this.ontologyFile = ontologyFile;
+
+        // Temporarily needed to explore HpoDiseases in test class because there is no adequate phenol documentation
+        this.diseases = getPhenolHpoDiseases(ontologyFile, phenotypeAnnotationFile);
+
+        // Create dictionary using Phenol
+        this.diseaseFeaturesDict = createDiseaseDictUsingPhenol(ontologyFile, phenotypeAnnotationFile);
+
+        // Create dictionary by parsing HPOA file phenotype.hpoa
+        //this.validDatabaseList = List.of("OMIM", "ORPHA", "DECIPHER");
+        this.validDatabaseList = List.of("OMIM");
+
+        this.hpoFreqTermList = List.of("HP:0040280", "HP:0040281", "HP:0040282", "HP:0040283", "HP:0040284", "HP:0040285");
+        this.excludedHpoFreqTermList = List.of("HP:0040280");
+    }
+
+    private HpoDiseases getPhenolHpoDiseases(String ontologyFile, String phenotypeAnnotationFile) throws IOException {
+        /*
+        Code required to get a kind of list of HpoDisease objects in Phenol from the HPOA file phenotype.hpoa
+        and the HP ontology in JSON format.
+         */
         Ontology ontology = OntologyLoader.loadOntology(new File(ontologyFile));
         HpoDiseaseLoader loader = HpoDiseaseLoaders.defaultLoader(ontology, HpoDiseaseLoaderOptions.defaultOmim());
-        this.diseases = loader.load(Paths.get(phenotypeAnnotationFile));
+        return loader.load(Paths.get(phenotypeAnnotationFile));
+    }
 
-        this.diseaseDict = new HashMap<String, List<String>>();
+    private HashMap<String, HashMap<String, Set<String>>> createDiseaseDictUsingPhenol(String ontologyFile, String phenotypeAnnotationFile) throws IOException {
+        HashMap<String, HashMap<String, Set<String>>> diseaseFeaturesDict = new HashMap<String, HashMap<String, Set<String>>>();
+        diseases = getPhenolHpoDiseases(ontologyFile, phenotypeAnnotationFile);
         for (HpoDisease disease : diseases) {
-            List<String> termIdList = disease.annotationTermIdList().stream()
+            Set<String> termIdSet = disease.annotationTermIdList().stream()
                     .map(object -> Objects.toString(object, null))
-                    .collect(Collectors.toList());
-            this.diseaseDict.put(disease.id().toString(), termIdList);
+                    .collect(Collectors.toSet());
+            HashMap<String, Set<String>> iTerms = new HashMap<String, Set<String>>();
+            iTerms.put("I", termIdSet);
+            diseaseFeaturesDict.putIfAbsent(disease.id().toString(), iTerms);
+        }
+        return diseaseFeaturesDict;
+    }
+
+    public HashMap<String, HashMap<String, Set<String>>> createDiseaseDictByParsing(String phenotypeAnnotationFile) {
+        HashMap<String, HashMap<String, Set<String>>> diseaseFeaturesDict = new HashMap<String, HashMap<String, Set<String>>>();
+        // Open HPOA file phenotype.hpoa
+        try {
+            File myObj = new File(phenotypeAnnotationFile);
+            Scanner myReader = new Scanner(myObj);
+            int cnt = 0;
+            while (myReader.hasNextLine()) {
+                String line = myReader.nextLine();
+                // Skip header lines
+                if (line.startsWith("#") | line.startsWith("database_id")) {
+                    continue;
+                }
+                String[] fields = line.split("\t");
+                if (fields.length != 12) {
+                    System.out.println("ERROR: Row does not have 12 fields!");
+                    break;
+                }
+                String disease_id = fields[0];
+                String hpo_id = fields[3];
+                String frequency = fields[7];
+                String database_tag = disease_id.split(":")[0];
+                if (!validDatabaseList.contains(database_tag)) {
+                    // Annotations can be restricted to individual databases from OMIM, ORPHA and DECIPHER.
+                    continue;
+                }
+                String HpoFreqTerm = this.freqStringToHpoTerm(frequency);
+                if (!diseaseFeaturesDict.containsKey(disease_id)) {
+
+                    diseaseFeaturesDict.putIfAbsent(disease_id, new HashMap<String, Set<String>>());
+                    diseaseFeaturesDict.get(disease_id).put("E", Collections.emptySet());
+                    diseaseFeaturesDict.get(disease_id).put("I", Collections.emptySet());
+                }
+                if (this.excludedHpoFreqTermList.contains(HpoFreqTerm)) {
+                    // Term is excluded
+                    diseaseFeaturesDict.get(disease_id).get("E").add(hpo_id);
+                } else {
+                    // Term is included
+                    diseaseFeaturesDict.get(disease_id).get("I").add(hpo_id);
+                }
+
+                cnt++;
+                if(cnt==100){
+                    continue;
+                    //break;
+                }
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        return diseaseFeaturesDict;
+    }
+
+    public double freqStringToFloat(String freqString) {
+        /*
+        Given an HPOA frequency string like "1/4", "20%" or “2.5”, return a float like 0.2, 0.205 or 0.25.
+        */
+        double freqFloat;
+        if (Pattern.matches("\\d{1,}/\\d{1,}", freqString)) {
+            // E.g., 1/4
+            double numerator = Integer.parseInt(freqString.split("/")[0]);
+            double denominator = Integer.parseInt(freqString.split("/")[1]);
+            freqFloat = numerator/denominator;
+        } else if (Pattern.matches("\\d{1,3}%", freqString)) {
+            // E.g., 20%
+            double percentage = Double.parseDouble(freqString.split("%")[0]);
+            freqFloat = percentage/100;
+        } else if (Pattern.matches("\\d{1,3}\\.\\d{1,20}%$", freqString)) {
+            // E.g., 2.5%
+            double percentage = Double.parseDouble(freqString.split("%")[0]);
+            freqFloat = percentage/100;
+        } else if (freqString.equals("")) {
+            // If no frequency is given, we assume a frequency of 1
+            freqFloat = 1.0;
+        } else {
+            freqFloat = -1.0;
+        }
+        // Check whether frequency is between 0 and 1
+        if (freqFloat < 0 | 1 < freqFloat) {
+            freqFloat = -1.0;
+        }
+        if (freqFloat == -1) {
+            System.out.println("ERROR: Invalid frequency string \"" + freqString + "\" from HPOA!");
+            System.exit(1);
+        }
+        return freqFloat;
+    }
+
+    public String freqStringToHpoTerm(String freqString) {
+        /*
+    Given an HPOA frequency string like “1/4”, “20%” or “2.5”, return one of the corresponding HPO frequency terms.
+         */
+
+        if (this.hpoFreqTermList.contains(freqString)) {
+            // Frequency already specified as HPO term
+            return freqString;
+        }
+        else {
+            double freqFloat = this.freqStringToFloat(freqString);
+
+            if (freqFloat == 1) {
+                // Obligate
+                return "HP:0040280";
+            } else if (freqFloat >= 0.8 && freqFloat < 1.0) {
+                // Very frequent
+               return "HP:0040281";
+            } else if (freqFloat >= 0.3 && freqFloat < 0.8) {
+                // Frequent
+                return "HP:0040282";
+            } else if (freqFloat >= 0.05 && freqFloat < 0.3) {
+                // Occasional
+                return "HP:0040283";
+            } else if (freqFloat >= 0.01 && freqFloat < 0.05) {
+                // Very rare
+                return "HP:0040284";
+            } else {
+                // Excluded
+                return "HP:0040285";
+            }
         }
     }
 
@@ -47,11 +200,15 @@ public class DiseaseDict {
         return this.diseases;
     }
 
-    public HashMap<String, List<String>> getDiseaseDict(){
-        return this.diseaseDict;
+    public HashMap<String, HashMap<String, Set<String>>> getDiseaseFeaturesDict(){
+        return this.diseaseFeaturesDict;
     }
 
-    public List<String> get_disease_features(String omimId){
-        return this.diseaseDict.get(omimId);
+    public Set<String> getIncludedDiseaseFeatures(String omimId){
+        return this.diseaseFeaturesDict.get(omimId).get("I");
+    }
+
+    public Set<String> getExcludedDiseaseFeatures(String omimId){
+        return this.diseaseFeaturesDict.get(omimId).get("E");
     }
 }
--- boqa-core/src/test/java/com/github/p2gx/boqa/core/DiseaseDictTest.java
+++ boqa-core/src/test/java/com/github/p2gx/boqa/core/DiseaseDictTest.java
@@ -2,10 +2,13 @@ package com.github.p2gx.boqa.core;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.monarchinitiative.phenol.annotations.base.Ratio;
 import org.monarchinitiative.phenol.annotations.formats.hpo.HpoDisease;
 import org.monarchinitiative.phenol.ontology.data.TermId;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,28 +20,192 @@ class DiseaseDictTest {
     static void setup() throws IOException {
         String annotationFile = "/Users/hansenp/development/BOQA/data/human-phenotype-ontology/latest_20250603/phenotype.hpoa";
         String ontologyFile = "/Users/hansenp/development/BOQA/data/human-phenotype-ontology/latest_20250603/hp.json";
-
         testDiseaseDict = new DiseaseDict(annotationFile, ontologyFile);
+    }
+
+    /*
+    Exploration of the parsing of the file phenotype.hpoa by Phenol
+     */
+
+    @Test
+    void getDiseases_onset() {
+        String omimId = "OMIM:618117";
+        System.out.println(omimId);
+        // OVARIAN DYSGENESIS 7; ODG7
+        // Juvenile onset (HP:0003621) is an annotated disease feature
+        HpoDisease disease = testDiseaseDict.getDiseases().diseaseById().get(TermId.of(omimId));
+        System.out.println("List of features: " + disease.annotationTermIdList());
+        System.out.println("HP:0003621 included in list: " + disease.annotationTermIdList().toString().contains("HP:0003621"));
+        System.out.println("Content of disease.diseaseOnset(): " + disease.diseaseOnset().toString());
+        // Juvenile onset (HP:0003621) is not in the list of annotated features
+        // Juvenile onset (HP:0003621) is a Clinical Modifier in HPO
+        // Description of Juvenile onset: Onset of signs or symptoms of disease between the age of 5 and 15 years.
+        // Content of disease.diseaseOnset(): 1827 to 5844 days, corresponds to 5 to 16 years.
+    }
+
+    @Test
+    void getDiseases_inheritance() {
+        String omimId = "OMIM:618117";
+        System.out.println(omimId);
+        // OVARIAN DYSGENESIS 7; ODG7
+        // Autosomal recessive inheritance (HP:0000007) is an annotated disease feature
+        HpoDisease disease = testDiseaseDict.getDiseases().diseaseById().get(TermId.of(omimId));
+        System.out.println("List of features: " + disease.annotationTermIdList());
+        System.out.println("HP:0000007 included in list: " + disease.annotationTermIdList().toString().contains("HP:0000007"));
+        System.out.println("Content of disease.modesOfInheritance(): " + disease.modesOfInheritance());
+        // Autosomal recessive inheritance (HP:0000007) is not in the list of annotated features
+        // Autosomal recessive inheritance (HP:0000007) is a Mode of Inheritance in HPO
+    }
+
+    @Test
+    void getDiseases_age_of_death() {
+        String omimId = "OMIM:617350";
+        System.out.println(omimId);
+        // DEVELOPMENTAL AND EPILEPTIC ENCEPHALOPATHY 52; DEE52
+        // Death in childhood (HP:0003819) is an annotated disease feature
+        HpoDisease disease = testDiseaseDict.getDiseases().diseaseById().get(TermId.of(omimId));
+        System.out.println("List of features: " + disease.annotationTermIdList());
+        System.out.println("HP:0003819 included in list: " + disease.annotationTermIdList().toString().contains("HP:0003819"));
+        // Death in childhood (HP:0003819) is not in the list of annotated features
+        // Death in childhood (HP:0003819) is a Clinical Modifier in HPO
+    }
+
+    @Test
+    void getDiseases_frequency_not_available() {
+        String omimId = "OMIM:609153";
+        // Frequencies of all included terms not available
+        // One term (HP:0001878) is excluded with a frequency of 0/2
+        System.out.println(omimId);
+        HpoDisease disease = testDiseaseDict.getDiseases().diseaseById().get(TermId.of(omimId));
+        for (TermId term : disease.annotationTermIdList()) {
+            Ratio ratio = disease.getFrequencyOfTermInDisease(term).get();
+            System.out.println(term.toString() + ": " + ratio);
+        }
+        // For the term with a frequency of 0/2 is converted to 0/2 (correct).
+        // All other terms are converted to 1/1 (correct).
+    }
+
+    @Test
+    void getDiseases_frequencies_as_percentages() {
+        String omimId = "OMIM:165500";
+        // In the file phenotype.hpoa the frequency of HP:0000486 is given as 10% and of HP:0000666 as 5%.
+        System.out.println(omimId);
+        HpoDisease disease = testDiseaseDict.getDiseases().diseaseById().get(TermId.of(omimId));
+        for (TermId term : disease.annotationTermIdList()) {
+            if (term.toString().equals("HP:0000486") | term.toString().equals("HP:0000666")) {
+                Ratio ratio = disease.getFrequencyOfTermInDisease(term).get();
+                System.out.println(term + ": " + ratio);
+            }
+        }
+        // The term HP:0000486 with 10% is converted to 1/5, which corresponds to 20% (incorrect).
+        // The term HP:0000486 with 5% is converted to 0/5, which corresponds to 0% (incorrect).
+    }
+
+    @Test
+    void getDiseases_frequencies_as_percentages_with_decimals() {
+        String omimId = "OMIM:609939";
+        // In the file phenotype.hpoa the frequency of HP:0000077 is given as 32.3% and of HP:0000992 as 76.3%.
+        System.out.println(omimId);
+        HpoDisease disease = testDiseaseDict.getDiseases().diseaseById().get(TermId.of(omimId));
+        List<String> relevantTermIds = List.of("HP:0000077", "HP:0000992", "HP:0000707");
+        for (TermId term : disease.annotationTermIdList()) {
+            if (relevantTermIds.contains(term.toString())) {
+                Ratio ratio = disease.getFrequencyOfTermInDisease(term).get();
+                System.out.println(term + ": " + ratio);
+            }
+        }
+        // The term HP:0000077 with 32.3%  is converted to 2/5, which corresponds to 40%.
+        // The term HP:0000992 with 76.3% is converted to 4/5, which corresponds to 80%.
+    }
+
+    @Test
+    void getDiseases_frequencies_as_hpo_frequency_term() {
+        String omimId = "OMIM:276820";
+        // In the file phenotype.hpoa the frequency of "HP:0009815", "HP:0001773", "HP:0002980", "HP:0002827"
+        // are given as HP:0040280, HP:0040281, HP:0040282, HP:0040283.
+        System.out.println(omimId);
+        HpoDisease disease = testDiseaseDict.getDiseases().diseaseById().get(TermId.of(omimId));
+        List<String> relevantTermIds = List.of("HP:0009815", "HP:0001773", "HP:0002980", "HP:0002827");
+        for (TermId term : disease.annotationTermIdList()) {
+            if (relevantTermIds.contains(term.toString())) {
+                Ratio ratio = disease.getFrequencyOfTermInDisease(term).get();
+                System.out.println(term + ": " + ratio);
+            }
+        }
+        // The term HP:0009815 with HP:0040280 is converted to 5/5, which corresponds to 100% (correct).
+        // The term HP:0001773 with HP:0040281 is converted to 4/5, which corresponds to 80% (correct).
+        // The term HP:0002980 with HP:0040282 is converted to 3/5, which corresponds to 60% (correct).
+        // The term HP:0002827 with HP:0040283 is converted to 1/5, which corresponds to 20% (correct).
+    }
+
+    /*
+    Tests for parsing
+     */
+
+    @Test
+    void testCreateDiseaseDictByParsing() {
+        testDiseaseDict.createDiseaseDictByParsing((testDiseaseDict.phenotypeAnnotationFile));
+    }
+
+    @Test
+    void testFreqStringToFloat() {
+        List<String> percentages = new ArrayList<>();
+        System.out.println("Percentages");
+        percentages.add("1%");
+        percentages.add("20%");
+        percentages.add("100%");
+        percentages.stream().forEach(s -> System.out.println(s + ": " + testDiseaseDict.freqStringToFloat(s)));
+        percentages.clear();
+        System.out.println("Percentages with decimals:");
+        percentages.add("1.2%");
+        percentages.add("20.123456%");
+        percentages.add("100.00%");
+        percentages.stream().forEach(s -> System.out.println(s + ": " + testDiseaseDict.freqStringToFloat(s)));
+        percentages.clear();
+        System.out.println("Ratios:");
+        percentages.add("1/1");
+        percentages.add("1/2");
+        percentages.add("0/4");
+        percentages.stream().forEach(s -> System.out.println(s + ": " + testDiseaseDict.freqStringToFloat(s)));
+        percentages.clear();
+        System.out.println("Special cases:");
+        percentages.add("");
+        //percentages.add("1000%");
+        //percentages.add("2/1");
+        percentages.stream().forEach(s -> System.out.println(s + ": " + testDiseaseDict.freqStringToFloat(s)));
+    }
+
+    @Test
+    void testFreqStringToHpoTerm() {
+        List<String> freqStringList = new ArrayList<String>();
+        System.out.println("Percentages:");
+        freqStringList.add("100%");
+        freqStringList.add("99.99%");
+        freqStringList.add("80%");
+        freqStringList.add("79.99%");
+        freqStringList.add("30%");
+        freqStringList.add("29.99%");
+        freqStringList.add("5%");
+        freqStringList.add("4.99%");
+        freqStringList.add("1%");
+        freqStringList.add("0.99%");
+        freqStringList.add("0%");
+        freqStringList.stream().forEach(s -> System.out.println(s + ": " + testDiseaseDict.freqStringToHpoTerm(s)));
+        freqStringList.clear();
+        System.out.println("Ratios:");
+        freqStringList.add("1/1");
+        freqStringList.add("999/1000");
+        freqStringList.add("4/5");
+        freqStringList.add("3999/5000");
+        freqStringList.add("0/1");
+        freqStringList.stream().forEach(s -> System.out.println(s + ": " + testDiseaseDict.freqStringToHpoTerm(s)));
 
     }
 
     @Test
-    void getDiseases() {
+    void getDiseases_size() {
         int size = testDiseaseDict.getDiseases().size();
         assertEquals(8362, size);
-
-        String omimId = "OMIM:604091";
-        System.out.println(omimId);
-        for (HpoDisease disease : testDiseaseDict.getDiseases()) {
-            if (disease.id().toString().equals(omimId)) {
-                System.out.println(disease.modesOfInheritance().toString());
-                System.out.println(disease.annotationTermIdList());
-                for (TermId termId : disease.annotationTermIdList()){
-                    System.out.println(termId);
-                    System.out.println(disease.getFrequencyOfTermInDisease(termId).get().numerator());
-                }
-            }
-        }
     }
 
     @Test
@@ -46,6 +213,9 @@ class DiseaseDictTest {
     }
 
     @Test
-    void get_disease_features() {
+    void testGetIncludedDiseaseFeatures() {
+        String omimId = "OMIM:618117";
+        System.out.println(omimId);
+        System.out.println(testDiseaseDict.getIncludedDiseaseFeatures(omimId));
     }
 }
