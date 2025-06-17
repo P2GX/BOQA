81f1ec31173853b8f506543c3b15abe08c6b1bec
Leonardo macOS <leonardochimirri94@gmail.com>
2025-06-17 19:09:08 +0200

    moved graph traversal to CounterSetApproach, basics seem fine for now, tests sitll todo

--- boqa-core/src/main/java/com/github/p2gx/boqa/core/CounterSetApproach.java
+++ boqa-core/src/main/java/com/github/p2gx/boqa/core/CounterSetApproach.java
@@ -1,25 +1,30 @@
 package com.github.p2gx.boqa.core;
 
 import org.monarchinitiative.phenol.ontology.data.TermId;
+import org.monarchinitiative.phenol.ontology.data.Ontology;
+import org.monarchinitiative.phenol.io.OntologyLoader;
 
+import java.io.File;
+import java.nio.file.Path;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class CounterSetApproach implements Counter{
 
-    private final String hpo; // not a string
+
+    private final Ontology hpoOntology;
     private Set<TermId> queryLayerInitialized;
     private Map<TermId, Set<TermId>> diseaseLayers = new HashMap<>();
 
     // for each disease in diseaseData compute ancestors OR load from disk (?) [--> .ser, serialize object]
-    public CounterSetApproach(Set<String> diseaseData, String hpo){
-        this.hpo = hpo; // ontology file
-        //NOTE diseaseData is a type/object
-        // for each d in diseaseData
-            // observedhpos = diseaseData.getDiseaseHpoTerms(d)
-            // diseaseLayers.put(d, initLayer(observedhpos))
-            // HashMap of Sets: each disease (key) associated to set of hpo terms (values are explicitly observed
-            // hpos AND ancestors)
+    public CounterSetApproach(DiseaseData diseaseData, Path hpoPath){
+        File hpoFile = hpoPath.toFile();
+        this.hpoOntology = OntologyLoader.loadOntology(hpoFile);
+        for (DiseaseData d : diseaseData){ // for each not applicable to type disease data, fix this
+            Set<String> observedHpos = d.getIncludedDiseaseFeatures(d.toString());
+            Set<TermId> observedHposTerms = observedHpos.stream().map(TermId::of).collect(Collectors.toSet());
+            diseaseLayers.put(TermId.of(d.toString()), initLayer(observedHposTerms));
+        }
     }
 
 
@@ -31,9 +36,13 @@ public class CounterSetApproach implements Counter{
         this.queryLayerInitialized = initLayer(queryTermIDs);
     }
 
-    Set<TermId> initLayer(Set<TermId> hpoterms){
-        // TODO implement real function
-        return hpoterms;
+    Set<TermId> initLayer(Set<TermId> hpoTerms){
+        List<TermId> observed = hpoTerms.stream().toList();
+        Set<TermId> observedAncestors = new HashSet<>();
+        for (TermId termId : observed) {
+            observedAncestors.addAll( hpoOntology.graph().extendWithAncestors(termId, true));
+        }
+        return observedAncestors;
     }
 
     //TODO
