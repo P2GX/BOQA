db072ee7103b4cfc63fd45f8136aea6560414cd5
Peter Robinson <peter.robinson@jax.org>
2026-08-12 14:34:06 +0200

    reverting interface change
    Added a method to create PatientData from a list of observed HPO terms.

--- boqa-core/src/main/java/org/p2gx/boqa/core/PatientData.java
+++ boqa-core/src/main/java/org/p2gx/boqa/core/PatientData.java
@@ -35,4 +35,14 @@ public interface PatientData {
         // If excluded terms are not used or not available
         return Set.of();
     }
+
+    /** The Exomiser provides us with a List of HPO identifiers as Strings. */
+    public static PatientData fromObservedHpoTermList(List<String> observed) {
+        String randomId = java.util.UUID.randomUUID().toString();
+        Set<TermId> observedTidSet = observed.stream()
+            .map(TermId::of)
+            .collect(toSet());
+        return new DefaultPatientData(randomId, observedTidSet);
+    }
+
 }
